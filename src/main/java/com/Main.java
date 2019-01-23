ebd03f2a80c366f4c1c4bf211f51a9a0ebff1a32
xiluhua <xlh505440@163.com>
2019-01-23 13:59:03 +0800

    add to https://github.com/xiluhua/jpa-spring.git

---
+++ src/main/java/com/Main.java
@@ -0,0 +1,23 @@
+package com;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.jpa.entity.Customer;
+import com.jpa.service.CustomerService;
+
+public class Main {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+		DataSource dataSource  = (DataSource)ctx.getBean("dataSource");
+		System.out.println(dataSource);
+		
+		Customer entity = new Customer("大姐F", 80);
+		CustomerService service = (CustomerService)ctx.getBean("customerImpl");
+		service.save(entity);
+	}
+}
---
+++ src/main/java/com/jpa/service/CustomerService.java
@@ -0,0 +1,8 @@
+package com.jpa.service;
+
+import com.jpa.entity.Customer;
+
+public interface CustomerService {
+
+	public void save(Customer entity);
+}
